25cbf5d57ac18c79d6467fb3cb37e437f329471c
bocaj111004 <bocaj111004@gmail.com>
2025-05-19 15:15:39 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,16 +1,39 @@
-local Settings = {
+local ScriptSettings = {
  ScriptName = "Jacob's Epic Script",
  genvAddress = "JES"
 }
 
-if getgenv()[Settings.genvAddress] then
- game:GetService("StarterGui"):SetCore("SendNotification",{
-		Title = Settings.ScriptName,
+getgenv()["_Settings"] = Settings
+
+if getgenv()[ScriptSettings.genvAddress] == true then
+ 
+	
+	game:GetService("StarterGui"):SetCore("SendNotification",{
+		Title = ScriptSettings.ScriptName,
 		Text = "Script has already been executed.",
 
 	})
  return
 
- getgenv()[Settings.genvAddress] = true
+ getgenv()[ScriptSettings.genvAddress] = true
+
+	local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+if not LocalPlayer.Character then
+	game:GetService("StarterGui"):SetCore("SendNotification",{
+		Title = ScriptSettings.ScriptName,
+		Text = "Script is waiting for the game to load.",
+
+	})
+		while not LocalPlayer.Character do
+		task.wait()
+	end
+		game:GetService("StarterGui"):SetCore("SendNotification",{
+		Title = ScriptSettings.ScriptName,
+		Text = "Script is now loading.",
+
+	})
+	end
 
 
