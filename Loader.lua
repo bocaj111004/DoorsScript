81b0ac60785fa24edb3b7b649628cb3a7965a078
bocaj111004 <bocaj111004@gmail.com>
2025-05-19 15:03:33 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,16 @@
-getgenv().Settings = {
- ScriptName = "Jacob's Epic Script"
+local Settings = {
+ ScriptName = "Jacob's Epic Script",
+ genvAddress = "JES"
 }
+
+if getgenv()[Settings.genvAddress] then
+ game:GetService("StarterGui"):SetCore("SendNotification",{
+		Title = Settings.ScriptName,
+		Text = "Script has already been executed.",
+
+	})
+ return
+
+ getgenv()[Settings.genvAddress] = true
+
+
